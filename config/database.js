afea1e0a854799caff2e4cb4573cd567e3e6b683
Pedro <pedrosiqueirasouza6@gmail.com>
2025-05-22 20:59:20 -0300

    fix: bug na configuração do db

--- config/database.js
+++ config/database.js
@@ -3,13 +3,19 @@ require('dotenv').config();
 
 const { Pool } = require('pg');
 
+const isSSL = process.env.DB_SSL === 'true';
+
 // Criando a pool de conexões
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
+  database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: isSSL ? { rejectUnauthorized: false } : false,
 });
 
-module.exports = pool;
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+  connect: () => pool.connect(),
+};
