d5670b091666a43300f1cf281c7105b7fbd37b44
Pedro <pedrosiqueirasouza6@gmail.com>
2025-05-22 21:01:11 -0300

    fix: atualizando script sql
    Atualizando o script com chave de id aleatoria

--- scripts/init.sql
+++ scripts/init.sql
@@ -1,42 +1,43 @@
-DROP TABLE IF EXISTS marca CASCADE;
-DROP TABLE IF EXISTS users CASCADE;
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
 DROP TABLE IF EXISTS carro CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS marca CASCADE;
 
 CREATE TABLE IF NOT EXISTS marca (
-    id INT PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     nome VARCHAR(100) NOT NULL,
     logo_url VARCHAR(255)
 );
 
 CREATE TABLE IF NOT EXISTS users (
-    id INT PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     senha VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS carro (
-    id INT PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     modelo VARCHAR(100) NOT NULL,
     potencia INT NOT NULL,
     ano_fabricacao INT,
-    marca_id INT,
-    user_id INT,
-    
+    marca_id UUID,
+    user_id UUID,
 
     FOREIGN KEY (marca_id) REFERENCES marca(id),
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
-INSERT INTO marca (id, nome, logo_url) VALUES
-(1, 'Toyota', 'https://upload.wikimedia.org/wikipedia/commons/4/44/Toyota.png'),
-(2, 'BMW', 'https://logospng.org/download/bmw/logo-bmw-2048.png'),
-(3, 'Audi', 'https://static.vecteezy.com/system/resources/previews/019/766/236/non_2x/audi-logo-audi-icon-transparent-free-png.png');
+INSERT INTO marca (nome, logo_url) VALUES
+('Toyota', 'https://upload.wikimedia.org/wikipedia/commons/4/44/Toyota.png'),
+('BMW', 'https://logospng.org/download/bmw/logo-bmw-2048.png'),
+('Audi', 'https://static.vecteezy.com/system/resources/previews/019/766/236/non_2x/audi-logo-audi-icon-transparent-free-png.png');
 
-INSERT INTO users (id, nome, email, senha) VALUES 
-(1, 'Admin Teste', 'admin@teste.com', '123456');
+INSERT INTO users (nome, email, senha) VALUES 
+('Admin Teste', 'admin@teste.com', '123456');
 
-INSERT INTO carro (id, modelo, potencia, ano_fabricacao, marca_id, user_id) VALUES 
-(1, 'Supra',324, 2023, 1, 1),
-(2, 'M3 Competition', 510, 2022, 2, 1),
-(3, 'RS6', 630, 2023, 3, 1);
+INSERT INTO carro (modelo, potencia, ano_fabricacao, marca_id, user_id) VALUES
+('Supra', 324, 2023, (SELECT id FROM marca WHERE nome = 'Toyota'), (SELECT id FROM users WHERE email = 'admin@teste.com')),
+('M3 Competition', 510, 2022, (SELECT id FROM marca WHERE nome = 'BMW'), (SELECT id FROM users WHERE email = 'admin@teste.com')),
+('RS6', 630, 2023, (SELECT id FROM marca WHERE nome = 'Audi'), (SELECT id FROM users WHERE email = 'admin@teste.com'));
