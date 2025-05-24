98fda8477cddb67e3529c41cfcd7bab1bd918b7a
Pedro <pedrosiqueirasouza6@gmail.com>
2025-05-24 16:30:19 -0300

    feat: add rota para listar todos users

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,9 @@
 const express = require('express');
 const router = express.Router();
+
+const UserController = require('../controllers/UserControllers.js')
+
+// Rota para listar todos os usuários
+router.get('/users', UserController.index)
+
 module.exports = router;
